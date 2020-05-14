4294c179016a5dce020414da18498ff331f55c5c
Artem66 <melodpi@gmail.com>
2020-05-14 15:53:40 +0300

    Updates

---
+++ precache-manifest.39e15651d2752963c5434afe7cfd6ede.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a913a0a3336c56d205dd5ab29e6b8aca",
+    "url": "/zazend/index.html"
+  },
+  {
+    "revision": "29e7e5a4a16a2c6bdcf9",
+    "url": "/zazend/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "3ec6f57d3c373861cb9d",
+    "url": "/zazend/static/css/main.a128d1be.chunk.css"
+  },
+  {
+    "revision": "29e7e5a4a16a2c6bdcf9",
+    "url": "/zazend/static/js/2.ed45301e.chunk.js"
+  },
+  {
+    "revision": "3ec6f57d3c373861cb9d",
+    "url": "/zazend/static/js/main.2f6eab56.chunk.js"
+  },
+  {
+    "revision": "32d64590f099cd5a88ae",
+    "url": "/zazend/static/js/runtime~main.e1b44d3c.js"
+  }
+]);
